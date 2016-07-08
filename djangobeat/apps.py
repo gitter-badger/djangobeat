1f2493ad69753ba5d77c4ce3e3119d2700804f63
Raja Simon <rajasimon@icloud.com>
2016-07-08 18:33:42 +0530

    advantage of ready function now support pre db sche

--- djangobeat/apps.py
+++ djangobeat/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from scheduler import Scheduler
 
 
 class DjangoBeatConfig(AppConfig):
     name = 'djangobeat'
     verbose_name = 'Django Beat'
+
+    def ready(self):
+        periodictask = self.get_model('PeriodicTask')
+        all_task = periodictask.objects.all()
+        for task in all_task:
+            sche = Scheduler(
+                schedule=task.schedule, channel_name=task.channel_name)
+            sche.agent()
