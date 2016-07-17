0dda9912316cc8adcf5e1b78670c947b50834269
Raja Simon <rajasimon@icloud.com>
2016-07-17 13:04:50 +0530

    Display error message when user doesn't setup database correctly

--- djangobeat/apps.py
+++ djangobeat/apps.py
@@ -1,3 +1,4 @@
+import logging
 from django.apps import AppConfig
 from scheduler import Scheduler
 
@@ -8,8 +9,15 @@ class DjangoBeatConfig(AppConfig):
 
     def ready(self):
         periodictask = self.get_model('PeriodicTask')
-        all_task = periodictask.objects.all()
-        for task in all_task:
-            sche = Scheduler(
-                schedule=task.schedule, channel_name=task.channel_name)
-            sche.agent()
+        try:
+            all_task = periodictask.objects.all()
+            for task in all_task:
+                sche = Scheduler(
+                    schedule=task.schedule, channel_name=task.channel_name)
+                sche.agent()
+        except:
+            logger = logging.getLogger('django')
+            logger.error(
+                'Currently Djangobeat support only with Database level beat' +
+                'config, please make sure your DB setup correctly!')
+            pass
